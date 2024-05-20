e8510f3d2291fc81fbe24efe66f0b05e17f3a657
fscelliott <42477011+fscelliott@users.noreply.github.com>
2024-05-20 10:50:26 -0600

    tests

--- scripts/check_links_cli.sh
+++ scripts/check_links_cli.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env npx zx
-// Check for broken links in the docs
-//await $`npx mintlify broken-links`.pipe(process.stdout);
+# Check for broken links in the docs
+# await $`npx mintlify broken-links`.pipe(process.stdout);
 
 npx mintlify broken-links
